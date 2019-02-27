fefbf3db2909b63c828a5386e8e2f2a4a85f5cb9
Brendan Gutleber <gutleberb@gmail.com>
2019-02-27 16:21:18 -0600

    setup array to hold number of correct answers and wrong answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,32 @@
 
-$(document).ready(function() {
-var timeRemaining = 60;
-var timer = setInterval(function () {
-    $("#timer").text(timeRemaining)
-    if (timeRemaining === 0) {
-        alert("Your time is up!!")
-    }
-    timeRemaining--;
-}, 1000)
+$(document).ready(function () {
+
+    var timeRemaining = 60;
+
+    var timer = setInterval(function () {
+        $("#timer").text(timeRemaining)
+        if (timeRemaining === 0) {
+            alert("Your time is up!!")
+            timeRemaining = 60;
+        }
+        timeRemaining--;
+    }, 1000)
 
 
 
-$("button").on("click", function () {
-    var userAnswers = [];
-    $.each($("input[type='checkbox']:checked"), function () {
-        userAnswers.push($(".form-check-input").val());
-        console.log(userAnswers)
-    });
+    $("button").on("click", function () {
+        var correctAnswers = [];
+        var wrongAnswers = [];
+        $.each($("input[type='checkbox']:checked"), function () {
+            if($(this).val() === "correct") {
+                correctAnswers.push($(this).val())
+            } else {
+                wrongAnswers.push($(this).val())
+            }
+            console.log(correctAnswers)
+            console.log(wrongAnswers)
+        });
 
-})
+    })
 
 });
