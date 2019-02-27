2c25ecc47006207d2bc23ae0fa603364b6490555
Brendan Gutleber <gutleberb@gmail.com>
2019-02-27 17:02:58 -0600

    added score tracker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,29 +2,38 @@
 $(document).ready(function () {
 
     var timeRemaining = 60;
+    var correctAnswers = 0;
+    var wrongAnswers = 0;
 
     var timer = setInterval(function () {
         $("#timer").text(timeRemaining)
         if (timeRemaining === 0) {
             alert("Your time is up!!")
             timeRemaining = 60;
+            displayScore();
+            clearInterval(timer);
         }
         timeRemaining--;
     }, 1000)
 
+    function displayScore() {
+        $("#score").text(correctAnswers);
+    }
 
 
     $("button").on("click", function () {
-        var correctAnswers = [];
-        var wrongAnswers = [];
+        correctAnswers = [];
+        wrongAnswers = [];
         $.each($("input[type='checkbox']:checked"), function () {
             if($(this).val() === "correct") {
-                correctAnswers.push($(this).val())
+                correctAnswers++;
             } else {
-                wrongAnswers.push($(this).val())
+                wrongAnswers++;
             }
             console.log(correctAnswers)
             console.log(wrongAnswers)
+            displayScore();
+            clearInterval(timer)
         });
 
     })
